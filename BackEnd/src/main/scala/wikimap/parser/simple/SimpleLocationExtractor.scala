f1382fbd967f8687872493dc7a26e118eae604a9
Misha wagner <mishajw@gmail.com>
2015-12-26 17:29:21 +0000

    More functionality

--- BackEnd/src/main/scala/wikimap/parser/simple/SimpleLocationExtractor.scala
+++ BackEnd/src/main/scala/wikimap/parser/simple/SimpleLocationExtractor.scala
@@ -1,25 +1,31 @@
 package wikimap.parser.simple
 
+import scala.util.matching.Regex.MatchIterator
+
 /**
   * Created by misha on 26/12/15.
   */
 object SimpleLocationExtractor {
 
+  private val nounGroup = "([A-Z][a-z]+ ?)+".r
+  private val link = "\\[\\[([^\\[[\\]]]+)\\]\\]".r
+
   def extractLocationMultiple(events: Seq[SimpleEvent]) =
     events.map(extractLocation)
 
   def extractLocation(event: SimpleEvent): Location = {
-
-
-
     LocationName("")
   }
 
-  private def extractNounsFromText(eventDesc: String): Seq[String] = {
-    stripLinkDetails(eventDesc)
-      .split("\\W")
-      .filter(_.startsWith("[A-Z]"))
-      .toSeq
+  def extractLinksFromText(text: String): Seq[String] = {
+    link.findAllIn(text)
+      .matchData
+      .flatMap(_.subgroups)
+      .toList.toSeq
+  }
+
+  def extractNounsFromText(text: String): Seq[String] = {
+    nounGroup.findAllIn(text).toList.toSeq
   }
 
   private def stripLinkDetails(eventDesc: String) =
