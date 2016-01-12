5e4eb53621ae33c2bd737aeb62f6f2e3db0de873
Misha wagner <mishajw@gmail.com>
2016-01-12 19:39:55 +0000

    Basic structure for taking in dates from URL

--- FrontEnd/app/controllers/Application.scala
+++ FrontEnd/app/controllers/Application.scala
@@ -1,26 +1,28 @@
 package controllers
 
-import models.IndexContainer
+import java.text.DateFormat
+
 import play.api.mvc._
 
-import scala.io.Source
+import scala.reflect.macros.ParseException
 
 class Application extends Controller {
+
+  private val dateFormat = new java.text.SimpleDateFormat("dd-MM-yyyy")
 
   def index = Action {
     Ok(views.html.index())
   }
 
-  def testGeocoder = Action { implicit request =>
-    Source.fromURL("")
+  def getEvents(startString: String, endString: String) = Action {
+    try {
+      val startDate = dateFormat.parse(startString)
+      val endDate = dateFormat.parse(endString)
 
-    Ok("")
+      Ok(s"$startDate - $endDate")
+    } catch {
+      case e: java.text.ParseException =>
+        Ok(s"Couldn't parse dates. Must be in DD-MM-YYYY format")
+    }
   }
-
-  def testIndexContainer = Action {
-    val a = new IndexContainer
-
-    Ok(a.getCompressedPosition("abel").toString)
-  }
-
 }
