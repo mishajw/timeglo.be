a4b337c2d0d7ad6bc8e956ca63ab32a9a19a447d
Misha wagner <mishajw@gmail.com>
2016-01-13 15:06:32 +0000

    Better error handling

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,8 @@ import org.json4s.jackson.JsonMethods
 import play.api.mvc._
 class Application extends Controller {
 
-  private val dateFormat = new java.text.SimpleDateFormat("dd.MM.yyyy")
+  private val dateFormatString: String = "dd.MM.yyyy"
+  private val dateFormat = new java.text.SimpleDateFormat(dateFormatString)
 
   def index = Action {
     Ok(views.html.index())
@@ -17,24 +18,37 @@ class Application extends Controller {
       val startDate = new java.sql.Date(dateFormat.parse(startString).getTime)
       val endDate = new java.sql.Date(dateFormat.parse(endString).getTime)
 
-      val events = JArray(DB.getLocatedEvents(startDate, endDate)
-        .map(le => {
-          JObject(List(
-            "date" -> JString(le.event.date.toString),
-            "desc" -> JString(le.event.description),
-            "location" -> JObject(List(
-              "names" -> JArray(le.location.names.map(JString).toList),
-              "lat" -> JDouble(le.location.coords.lat),
-              "long" -> JDouble(le.location.coords.long),
-              "population" -> JString(le.location.population.toString)
-            ))
-          ))
-        }).toList)
-
-      Ok(JsonMethods.pretty(JsonMethods.render(events)))
+      if (startDate.before(endDate)) {
+        Ok(stringifyJson(getEventsInJson(startDate, endDate)))
+      } else {
+        errorJson("Start date must be before end date")
+      }
     } catch {
       case e: java.text.ParseException =>
-        Ok(s"Couldn't parse dates. Must be in DD-MM-YYYY format")
+        errorJson(s"Not a valid format for a date. Must be in format ${dateFormatString.toUpperCase()}")
     }
   }
+
+  private def getEventsInJson(startDate: java.sql.Date, endDate: java.sql.Date) = {
+    JArray(DB.getLocatedEvents(startDate, endDate)
+      .map(le => {
+        JObject(List(
+          "date" -> JString(le.event.date.toString),
+          "desc" -> JString(le.event.description),
+          "location" -> JObject(List(
+            "names" -> JArray(le.location.names.map(JString).toList),
+            "lat" -> JDouble(le.location.coords.lat),
+            "long" -> JDouble(le.location.coords.long),
+            "population" -> JString(le.location.population.toString)
+          ))
+        ))
+      }).toList)
+  }
+
+  private def stringifyJson(json: JValue) =
+    JsonMethods.compact(JsonMethods.render(json))
+
+  private def errorJson(errorMsg: String) = BadRequest(stringifyJson(JObject(List(
+    "error" -> JString(errorMsg))
+  )))
 }
