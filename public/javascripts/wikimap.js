8cb4a3f11f3d9e6c5b4bc94f17d9b5ce7fa01543
Misha wagner <mishajw@gmail.com>
2016-01-16 14:35:39 +0000

    Put inside JQuery function for no globals

--- public/javascripts/wikimap.js
+++ public/javascripts/wikimap.js
@@ -1,255 +1,256 @@
+$(function() {
+    // Container for the visualisation
+    var $container = $("#wikimap-d3");
 
-// Container for the visualisation
-var $container = $("#wikimap-d3");
+    // D3 VARIABLES
+    var width = $container.width(),
+        height = $container.height();
 
-// D3 VARIABLES
-var width = $container.width(),
-    height = $container.height();
+    var projection = d3.geo.orthographic()
+        .scale(height / 2)
+        .translate([width / 2, height / 2])
+        .clipAngle(90);
 
-var projection = d3.geo.orthographic()
-    .scale(height / 2)
-    .translate([width / 2, height / 2])
-    .clipAngle(90);
+    var path = d3.geo.path()
+        .projection(projection);
 
-var path = d3.geo.path()
-    .projection(projection);
+    var λ = d3.scale.linear()
+        .domain([0, width])
+        .range([-180, 180]);
 
-var λ = d3.scale.linear()
-    .domain([0, width])
-    .range([-180, 180]);
+    var φ = d3.scale.linear()
+        .domain([0, height])
+        .range([90, -90]);
 
-var φ = d3.scale.linear()
-    .domain([0, height])
-    .range([90, -90]);
-
-var svg = d3.select("#wikimap-d3").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-
-// JQUERY VARS
-var $svg = $container.find("svg");
-var $tooltip = $container.find("#tooltip");
-var $slider = $("#range-slider");
-
-// MOUSE VARS
-var isMouseDown = false;
-var tooltipHasMouse = false;
-var mouseDownLocation = {x: 0, y: 0};
-var mouseLocation = {x: 0, y: 0};
-
-// GLOBE VARS
-var globeRotation = {x: 670, y: 400};
-var globeRotIncrement = 30;
-
-// OTHER
-var startYear = 2012, endYear = 2014;
-
-updateRotation();
-$.ajax("/getDateRange", {
-    success: function(e) {
-        var json = $.parseJSON(e);
-        setupSlider(json.startDate.year, json.endDate.year);
-    }
-});
-
-// LOADING DATA
-d3.json("/assets/res/world-110m.json", function(error, world) {
-    if (error) throw error;
-
-    svg.append("path")
-        .datum(topojson.feature(world, world.objects.land))
-        .attr("class", "land")
-        .attr("d", path);
-});
-
-updateWithRange(2010, 2012);
+    var svg = d3.select("#wikimap-d3").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
 
-// MOUSE/KEYBOARD EVENTS
-$svg.on("mousemove", function(e) {
-    mouseLocation.x = e.clientX;
-    mouseLocation.y = e.clientY;
+    // JQUERY VARS
+    var $svg = $container.find("svg");
+    var $tooltip = $container.find("#tooltip");
+    var $slider = $("#range-slider");
 
-    if (!isMouseDown) return;
+    // MOUSE VARS
+    var isMouseDown = false;
+    var tooltipHasMouse = false;
+    var mouseDownLocation = {x: 0, y: 0};
+    var mouseLocation = {x: 0, y: 0};
 
-    globeRotation.x += e.clientX - mouseDownLocation.x;
-    globeRotation.y += e.clientY - mouseDownLocation.y;
+    // GLOBE VARS
+    var globeRotation = {x: 670, y: 400};
+    var globeRotIncrement = 30;
 
-    mouseDownLocation.x = e.clientX;
-    mouseDownLocation.y = e.clientY;
+    // OTHER
+    var startYear = 2012, endYear = 2014;
 
     updateRotation();
-});
-
-$svg.on("mousedown", function(e) {
-    isMouseDown = true;
-
-    mouseDownLocation.x = e.clientX;
-    mouseDownLocation.y = e.clientY;
-});
-
-$svg.on("mouseup", function(e) {
-    isMouseDown = false;
-});
-
-function eventMouseOver(d) {
-    $tooltip.html(getText(d));
-    $tooltip.show();
-    $tooltip.css({
-        left: mouseLocation.x - ($tooltip.width() / 2),
-        top: mouseLocation.y
-    });
-}
-
-function eventMouseOut(d) {
-    $tooltip.fadeOut();
-}
-
-$tooltip.on("mouseover", function() {
-    tooltipHasMouse = true;
-});
-
-$tooltip.on("mouseout", function() {
-    tooltipHasMouse = false;
-});
-
-$(document).keydown(function(e) {
-    switch (e.which) {
-        case 37:
-            globeRotation.x += globeRotIncrement;
-            break;
-        case 38:
-            globeRotation.y += globeRotIncrement;
-            break;
-        case 39:
-            globeRotation.x -= globeRotIncrement;
-            break;
-        case 40:
-            globeRotation.y -= globeRotIncrement;
-            break;
-        case 27:
-            getNextPoints();
-            break;
-        default:
-            console.log(e.which);
-            break;
-    }
-
-    updateRotation();
-});
-
-
-// OTHER FUNCTIONS
-function updateWithRange() {
-    console.log("Getting points for " + startYear + " to " + endYear);
-
-    $.ajax("/getEvents/1.1." + startYear + "/1.1." + endYear + 10, {
-        type: "GET",
+    $.ajax("/getDateRange", {
         success: function(e) {
-            handleEvents($.parseJSON(e));
-        },
-        error: function(e) {
-            console.log("Couldn't get events.");
-            console.log(e);
+            var json = $.parseJSON(e);
+            setupSlider(json.startDate.year, json.endDate.year);
         }
     });
-}
 
-function handleEvents(events) {
-    console.log("Handling events");
-
-    svg.selectAll(".points").remove();
-
-    var topojsonObject = {
-        type: "Topology",
-        objects: {
-            events: {
-                type: "MultiPoint",
-                coordinates: []
-            }
-        },
-        arcs: [],
-        transform: {
-            scale: [1, 1],
-            translate: [0, 0]
-        }
-    };
-
-    events.forEach(function(e) {
-        // Set the topojson object to have details for this event
-        topojsonObject.objects.events.coordinates = [[
-            e.location.long,
-            e.location.lat,
-            // Inject into coordinates so we can get the data back later
-            e
-        ]];
+    // LOADING DATA
+    d3.json("/assets/res/world-110m.json", function(error, world) {
+        if (error) throw error;
 
         svg.append("path")
-            .datum(topojson.feature(topojsonObject, topojsonObject.objects.events))
-            .attr("class", "points")
-            .attr("fill", "red")
-            .attr("opacity", 0.5)
-            .on("mouseover", function(e) {
-                eventMouseOver(e.geometry.coordinates[0][2]);
-            })
-            .on("mouseout", function(e) {
-                eventMouseOut(e.geometry.coordinates[0][2]);
-            })
+            .datum(topojson.feature(world, world.objects.land))
+            .attr("class", "land")
             .attr("d", path);
     });
-}
 
-function setupSlider(min, max) {
-    function updateLabel() {
-        $("#range-label").text(
-            $slider.slider("values", 0) + " to " +
-            $slider.slider("values", 1));
-    }
+    updateWithRange(2010, 2012);
 
-    $slider.slider({
-        range: true,
-        min: min,
-        max: max,
-        values: [max - 1, max],
-        slide: function(event, ui) {
-            updateLabel();
-            startYear = ui.values[0];
-            endYear = ui.values[1];
-        }
+
+    // MOUSE/KEYBOARD EVENTS
+    $svg.on("mousemove", function(e) {
+        mouseLocation.x = e.clientX;
+        mouseLocation.y = e.clientY;
+
+        if (!isMouseDown) return;
+
+        globeRotation.x += e.clientX - mouseDownLocation.x;
+        globeRotation.y += e.clientY - mouseDownLocation.y;
+
+        mouseDownLocation.x = e.clientX;
+        mouseDownLocation.y = e.clientY;
+
+        updateRotation();
     });
 
-    updateLabel();
+    $svg.on("mousedown", function(e) {
+        isMouseDown = true;
 
-    $("#range-button").click(updateWithRange);
-}
+        mouseDownLocation.x = e.clientX;
+        mouseDownLocation.y = e.clientY;
+    });
 
-function updateRotation() {
-    if (globeRotation.y > 800) globeRotation.y = 800;
-    if (globeRotation.y < 0)   globeRotation.y = 0;
+    $svg.on("mouseup", function(e) {
+        isMouseDown = false;
+    });
 
-    projection.rotate([λ(globeRotation.x), φ(globeRotation.y)]);
-    svg.selectAll("path").attr("d", path);
-}
+    function eventMouseOver(d) {
+        $tooltip.html(getText(d));
+        $tooltip.show();
+        $tooltip.css({
+            left: mouseLocation.x - ($tooltip.width() / 2),
+            top: mouseLocation.y
+        });
+    }
 
-function getText(e) {
-    var date =
-        "<div class='event-date'>" +
-            e.date +
-        "</div>";
+    function eventMouseOut(d) {
+        $tooltip.fadeOut();
+    }
 
-    var desc =
-        "<div class='event-desc'>" +
-            formatDescription(e.desc) +
-        "</div>";
+    $tooltip.on("mouseover", function() {
+        tooltipHasMouse = true;
+    });
 
-    return date + desc;
-}
+    $tooltip.on("mouseout", function() {
+        tooltipHasMouse = false;
+    });
 
-function formatDescription(desc) {
-    var regex1 = /\[\[([^\[\|\]]*)\|([^\[\|\]]*)\]\]/g;
-    var regex2 = /\[\[([^\[\|\]]*)\]\]/g;
-    return desc
-        .replace(regex1, "<a href='http://en.wikipedia.org/wiki/$1'>$2</a>")
-        .replace(regex2, "<a href='http://en.wikipedia.org/wiki/$1'>$1</a>");
-}
+    $(document).keydown(function(e) {
+        switch (e.which) {
+            case 37:
+                globeRotation.x += globeRotIncrement;
+                break;
+            case 38:
+                globeRotation.y += globeRotIncrement;
+                break;
+            case 39:
+                globeRotation.x -= globeRotIncrement;
+                break;
+            case 40:
+                globeRotation.y -= globeRotIncrement;
+                break;
+            case 27:
+                getNextPoints();
+                break;
+            default:
+                console.log(e.which);
+                break;
+        }
+
+        updateRotation();
+    });
+
+
+    // OTHER FUNCTIONS
+    function updateWithRange() {
+        console.log("Getting points for " + startYear + " to " + endYear);
+
+        $.ajax("/getEvents/1.1." + startYear + "/1.1." + endYear + 10, {
+            type: "GET",
+            success: function(e) {
+                handleEvents($.parseJSON(e));
+            },
+            error: function(e) {
+                console.log("Couldn't get events.");
+                console.log(e);
+            }
+        });
+    }
+
+    function handleEvents(events) {
+        console.log("Handling events");
+
+        svg.selectAll(".points").remove();
+
+        var topojsonObject = {
+            type: "Topology",
+            objects: {
+                events: {
+                    type: "MultiPoint",
+                    coordinates: []
+                }
+            },
+            arcs: [],
+            transform: {
+                scale: [1, 1],
+                translate: [0, 0]
+            }
+        };
+
+        events.forEach(function(e) {
+            // Set the topojson object to have details for this event
+            topojsonObject.objects.events.coordinates = [[
+                e.location.long,
+                e.location.lat,
+                // Inject into coordinates so we can get the data back later
+                e
+            ]];
+
+            svg.append("path")
+                .datum(topojson.feature(topojsonObject, topojsonObject.objects.events))
+                .attr("class", "points")
+                .attr("fill", "red")
+                .attr("opacity", 0.5)
+                .on("mouseover", function(e) {
+                    eventMouseOver(e.geometry.coordinates[0][2]);
+                })
+                .on("mouseout", function(e) {
+                    eventMouseOut(e.geometry.coordinates[0][2]);
+                })
+                .attr("d", path);
+        });
+    }
+
+    function setupSlider(min, max) {
+        function updateLabel() {
+            $("#range-label").text(
+                $slider.slider("values", 0) + " to " +
+                $slider.slider("values", 1));
+        }
+
+        $slider.slider({
+            range: true,
+            min: min,
+            max: max,
+            values: [max - 1, max],
+            slide: function(event, ui) {
+                updateLabel();
+                startYear = ui.values[0];
+                endYear = ui.values[1];
+            }
+        });
+
+        updateLabel();
+
+        $("#range-button").click(updateWithRange);
+    }
+
+    function updateRotation() {
+        if (globeRotation.y > 800) globeRotation.y = 800;
+        if (globeRotation.y < 0)   globeRotation.y = 0;
+
+        projection.rotate([λ(globeRotation.x), φ(globeRotation.y)]);
+        svg.selectAll("path").attr("d", path);
+    }
+
+    function getText(e) {
+        var date =
+            "<div class='event-date'>" +
+                e.date +
+            "</div>";
+
+        var desc =
+            "<div class='event-desc'>" +
+                formatDescription(e.desc) +
+            "</div>";
+
+        return date + desc;
+    }
+
+    function formatDescription(desc) {
+        var regex1 = /\[\[([^\[\|\]]*)\|([^\[\|\]]*)\]\]/g;
+        var regex2 = /\[\[([^\[\|\]]*)\]\]/g;
+        return desc
+            .replace(regex1, "<a href='http://en.wikipedia.org/wiki/$1'>$2</a>")
+            .replace(regex2, "<a href='http://en.wikipedia.org/wiki/$1'>$1</a>");
+    }
+});
