b2fdde592763987d8aac1146e772c9359a9098b1
Misha wagner <mishajw@gmail.com>
2016-01-29 12:26:51 +0000

    New case class for basic locations

--- app/backend/backend.scala
+++ app/backend/backend.scala
@@ -4,6 +4,7 @@
 package object backend {
   case class Coords(lat: Double, long: Double)
   case class Location(formattedName: String, matchedNames: Seq[String], coords: Coords, population: java.math.BigDecimal, id: Option[Int] = None)
+  case class SimpleLocation(name: String, coords: Coords)
 
   case class Event(date: Date, description: String, id: Option[Int] = None)
   case class Date(date: Int, month: Int, year: Int)
