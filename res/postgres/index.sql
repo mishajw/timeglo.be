f59110e8fb7bc76b527487d968694561b27efb00
Misha wagner <mishajw@gmail.com>
2016-01-30 15:42:56 +0000

    Added indexes for new tables from wikipeida

--- res/postgres/index.sql
+++ res/postgres/index.sql
@@ -10,3 +10,6 @@ CREATE INDEX el_locid_idx   ON eventLocations   (locationID);
 
 CREATE INDEX el_evid_idx    ON eventLocations   (eventID);
 
+CREATE INDEX page_title_idx ON page             (page_title);
+
+CREATE INDEX geo_page_idx   ON geo_tags         (gt_page_id);
