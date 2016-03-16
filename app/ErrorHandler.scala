5065162d8714b29b76671dcd428dbcf8dfc9655c
Misha Wagner <mishajw@gmail.com>
2016-03-16 14:20:47 +0000

    Error page created

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,19 @@
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+
+class ErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    Future.successful(
+      Status(statusCode)(views.html.error(message, statusCode))
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
