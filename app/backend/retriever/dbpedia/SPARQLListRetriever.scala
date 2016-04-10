6dc5312374855bffe349b5944ba482aa43461bda
Misha Wagner <mishajw@gmail.com>
2016-04-10 21:08:04 +0100

    SPARQL results parsing

--- app/backend/retriever/dbpedia/SPARQLListRetriever.scala
+++ app/backend/retriever/dbpedia/SPARQLListRetriever.scala
@@ -2,18 +2,59 @@ package backend.retriever.dbpedia
 
 import java.net.URLEncoder
 
-import backend.Event
+import backend._
 
 import scala.io.Source
+
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 object SPARQLListRetriever {
 
   def run: Seq[Event] = {
-    val res = Source.fromURL(url(query)).mkString
+    val raw = getRaw
 
-    print(res)
+    val json = parse(raw)
+
+    val parsed = parseJson(json)
+
+    println(parsed.mkString("\n"))
 
     Seq()
+  }
+
+  private def parseJson(json: JValue): List[List[String]] = {
+
+    def getValue(obj: List[(String, JValue)]): String = {
+      (for { JField("value", JString(value)) <- obj } yield value).head
+    }
+
+    (for {
+      JObject(obj: List[(String, JsonAST.JValue)]) <- json
+      JField("results", JObject(results)) <- obj
+      JField("bindings", JArray(bindings)) <- results
+      JObject(eventContainer) <- bindings
+      JField("date", JObject(date)) <- eventContainer
+      JField("place_name", JObject(placeName)) <- eventContainer
+      JField("long", JObject(long)) <- eventContainer
+      JField("lat", JObject(lat)) <- eventContainer
+      JField("desc", JObject(desc)) <- eventContainer
+    } yield {
+      LocatedEvent(
+        Event(
+          Date(1, 1, 1),
+          getValue(desc)),
+        SimpleLocation(
+          getValue(placeName),
+          Coords(getValue(lat).toDouble, getValue(long).toDouble),
+          "")
+      )
+    }).asInstanceOf[List[List[String]]]
+  }
+
+  private def getRaw: String = {
+//    Source.fromURL(url(query)).mkString
+    Source.fromFile("largeresources/dbpedia.json").mkString
   }
 
   private def url(query: String): String = {
