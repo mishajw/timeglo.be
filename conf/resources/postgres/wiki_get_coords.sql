2be10b99296506c17fa533df0d30bcd0c7ebcdf2
Misha Wagner <mishajw@gmail.com>
2016-04-10 23:20:28 +0100

    Delete unused entries in roots

--- conf/resources/postgres/wiki_get_coords.sql
+++
@@ -1,5 +0,0 @@
-SELECT g.gt_name AS geo_name, p.page_title AS page_name, g.gt_lat, g.gt_lon
-FROM page p, geo_tags g
-WHERE
-  p.page_title = ? AND
-  g.gt_page_id = p.page_id;
--- conf/resources/postgres/wiki_get_loc_id.sql
+++
@@ -1,5 +0,0 @@
-SELECT g.gt_id
-FROM page p, geo_tags g
-WHERE
-  p.page_title = ? AND
-  g.gt_page_id = p.page_id;
