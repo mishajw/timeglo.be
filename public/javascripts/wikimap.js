52ac862b9ac7886957ea4dbdc56a442a8989d112
Misha Wagner <mishajw@gmail.com>
2016-04-12 23:47:33 +0100

    Start on top of europe and africa

--- public/javascripts/wikimap.js
+++ public/javascripts/wikimap.js
@@ -13,7 +13,7 @@ function Graph() {
     // GLOBE VARS
     var globeSize = Math.min(height, width) * 0.4;
     var globeSea = undefined;
-    var globeRotation = {x: 670, y: 400};
+    var globeRotation = {x: 558, y: 579};
     var desGlobeRotation = {x: globeRotation.x, y: globeRotation.y};
     var globeRotCatchUp = 0.3;
 
